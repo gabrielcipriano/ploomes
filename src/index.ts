d623acdb1139b02b2f64f0da9b4f74a0d3a24e0a
Gabriel Cipriano <gabrielfcipriano@gmail.com>
2023-03-19 23:50:12 -0300

    chore: add index

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/60924
+// declare global {
+//   const fetch: typeof import("undici").fetch;
+//   const Headers: typeof import("undici").Headers;
+//   const Request: typeof import("undici").Request;
+//   const Response: import("undici").Response;
+// }
+
+export * from './http';
+
+// export { PloomesClient } from "./PloomesClient.js";
+// export { Contact } from "./Entities.js";
+// export { CountryId, PhoneTypeId } from "./Consts.js";
